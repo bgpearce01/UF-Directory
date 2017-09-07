63c7ec574dc625a79b29eca16aeee1ccdf520e01
Brian Pearce <bgpearce01@gmail.com>
2017-09-06 21:38:15 -0400

    Added additional details box

--- listingController.js
+++ listingController.js
@@ -8,13 +8,25 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       as described in the assignment spec. 
      */
     $scope.addListing = function() {
-
+        Listings.unshift({
+              code: $scope.addItem.code.toUpperCase(),
+              name: $scope.addItem.name,
+              address: $scope.addItem.address,
+              coordinates: {
+                latitude: $scope.addItem.latitude,
+                longitude: $scope.addItem.longitude,
+              }
+            });
     };
     $scope.deleteListing = function(index) {
       Listings.splice(index, 1); //splice to delete listing 
     };
     $scope.showDetails = function(index) {
-      
+      $scope.detailedInfo = {
+          name:  Listings[index].code + " " +  Listings[index].name,
+          address: Listings[index].address,
+          coordinates: "Longitude: " + Listings[index].coordinates.longitude + ", Latitude: " + Listings[index].coordinates.latitude
+        }
     };
   }
 ]);
